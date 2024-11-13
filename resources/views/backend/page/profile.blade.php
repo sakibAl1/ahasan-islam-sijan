25a12e6326ad7126be20fc996db048a9e025d55c
Swapna201 <swapnakhatun865@gmail.com>
2024-11-13 16:23:08 +0600

    f

---
+++ resources/views/backend/page/profile.blade.php
@@ -0,0 +1,26 @@
+@extends('backend.layouts.master')
+@section('content')
+<main class="app-main"> <!--begin::App Content Header-->
+            <div class="app-content-header"> <!--begin::Container-->
+                <div class="container-fluid"> <!--begin::Row-->
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h3 class="mb-0">Profile</h3>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-end">
+                                <li class="breadcrumb-item"><a href="{{url('dashboard')}}">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    Profile
+                                </li>
+                            </ol>
+                        </div>
+                    </div> <!--end::Row-->
+                </div> <!--end::Container-->
+            </div> <!--end::App Content Header-->
+             <!--begin::App Content-->
+           <h1>Name={{Auth::user()->name}}</h1>
+           <h1>Email={{Auth::user()->email}}</h1>
+           <h1>Password={{Auth::user()->password}}</h1>
+        </main> <!--end::App Main--> 
+        @endsection
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/dashboard', function () {
+    return view('backend/dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+//frontend
+Route::get('/',[FrontController::class,'index'])->name('/');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    //add product
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    //product store
+    Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
+    //edit update
+    Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
+    //delete
+    Route::delete('/product/delete/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+});
+
+require __DIR__.'/auth.php';
